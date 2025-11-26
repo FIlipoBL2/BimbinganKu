2b872d65b06f262a4c4ca664b46a88b926f814fe
FilipoBL2 <6182301004@student.unpar.ac.id>
2025-11-26 13:14:43 +0700

    Initialize Repo

---
+++ src/main/java/com/RPL/BimbinganKu/BimbinganKuApplication.java
@@ -0,0 +1,13 @@
+package com.RPL.BimbinganKu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimbinganKuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimbinganKuApplication.class, args);
+	}
+
+}
