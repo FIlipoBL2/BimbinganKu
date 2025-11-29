7a812c65a4abcd304790e7298eb55ba2ff20fe2e
VinceFarrelNatanael <6182301024@student.unpar.ac.id>
2025-11-29 12:23:17 +0700

    recover test directory

---
+++ src/main/test/java/com/RPL/BimbinganKu/BimbinganKuApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.RPL.BimbinganKu;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class BimbinganKuApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
