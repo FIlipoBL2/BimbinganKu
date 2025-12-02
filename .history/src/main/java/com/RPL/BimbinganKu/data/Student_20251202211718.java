7d99f508f3d8bf078132bbe8326cae98a9b7d40f
VinceFarrelNatanael <6182301024@student.unpar.ac.id>
2025-12-02 22:22:53 +0700

    added password trimming

---
+++ .history/src/main/java/com/RPL/BimbinganKu/data/Student_20251202211718.java
@@ -0,0 +1,31 @@
+package com.RPL.BimbinganKu.data;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Student extends User {
+    @NotBlank
+    @Size(min = 10, max = 10)
+    private String npm;
+
+    private int totalGuidanceUTS;
+    private int totalGuidanceUAS;
+
+    public Student(String email, String password, String name, String npm, int uasGuidance, int utsGuidance) {
+        super(email, password, name);
+        this.npm = npm;
+        this.totalGuidanceUAS = uasGuidance;
+        this.totalGuidanceUTS = utsGuidance;
+    }
+
+    public Student(User user, String npm, int uasGuidance, int utsGuidance) {
+        super(user);
+        this.npm = npm;
+        this.totalGuidanceUAS = uasGuidance;
+        this.totalGuidanceUTS = utsGuidance;
+    }
+}
