ccbbac46909d9288133cfe9fc90cb0eee06655cb
Filipo Bintang Lautan <6182301004@student.unpar.ac.id>
2025-12-02 21:59:35 +0700

    fix dummy.sql

--- dbQuery/dummy.sql
+++ dbQuery/dummy.sql
@@ -1,13 +1,13 @@
 -- Insert dummy data into Users table
 -- All passwords are "password123" hashed with Bcrypt
-INSERT INTO Users (name, email, password) VALUES 
-('Gede Karyana', 'Gede@university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm'),
-('Husnul Hakim', 'Husnul@university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm'),
-('Elisati', 'Elisati@university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm'),
-('Filipo Bintang Lautan', '6182301004@student.university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm'),
-('Vince Farrel Nathaniel', '6182301024.brown@university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm'),
-('Basilius Mozes', '6182301032@student.university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm'),
-('Womby', '6182101099@student.university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm');
+INSERT INTO Users (id, name, email, password) VALUES 
+('GDK', 'Gede Karyana', 'Gede@university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm'),
+('HUH', 'Husnul Hakim', 'Husnul@university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm'),
+('ELH', 'Elisati', 'Elisati@university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm'),
+('6182301004','Filipo Bintang Lautan', '6182301004@student.university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm'),
+('6182301024','Vince Farrel Nathaniel', '6182301024.brown@university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm'),
+('6182301032','Basilius Mozes', '6182301032@student.university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm'),
+('6182101099','Womby', '6182101099@student.university.edu', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcg7b3XeKeUxWdeS86E36jbMFSm');
 
 -- Insert dummy data into Students table
 INSERT INTO Students (NPM, totalGuidanceUTS, totalGuidanceUAS) VALUES
@@ -23,17 +23,18 @@ INSERT INTO Lecturers (lecturerCode) VALUES
 ('ELH');
 
 -- Insert dummy data into Akademik table
-INSERT INTO Akademik (year, semester) VALUES
-('2024', 'Ganjil'),
-('2024', 'Genap'),
-('2023', 'Ganjil');
+INSERT INTO Akademik (akademik_ID, year, semester) VALUES
+(1, '2023', 'Ganjil'),
+(2, '2023', 'Genap'),
+(3, '2024', 'Ganjil'),
+(4, '2024', 'Genap');
 
 -- Insert dummy data into StudentSchedule table
 INSERT INTO StudentSchedule (NPM, akademik_ID, day, hourStart, hourEnd) VALUES
-(1, 1, 'Senin', '08:00:00', '10:00:00'),
-(1, 1, 'Rabu', '10:00:00', '12:00:00'),
-(2, 1, 'Selasa', '09:00:00', '11:00:00'),
-(5, 2, 'Kamis', '13:00:00', '15:00:00');
+(6182301004, 1, 'Senin', '08:00:00', '10:00:00'),
+(6182301024, 1, 'Rabu', '10:00:00', '12:00:00'),
+(6182301032, 1, 'Selasa', '09:00:00', '11:00:00'),
+(6182101099, 2, 'Kamis', '13:00:00', '15:00:00');
 
 -- Insert dummy data into LecturerSchedule table
 INSERT INTO LecturerSchedule (lecturerCode, akademik_ID, day, hourStart, hourEnd) VALUES
@@ -44,10 +45,10 @@ INSERT INTO LecturerSchedule (lecturerCode, akademik_ID, day, hourStart, hourEnd
 
 -- Insert dummy data into Topic table
 INSERT INTO Topic (topicCode, NPM, lecturerCode, topicName) VALUES
-('GDK5801ADS', 6182301004, 'GDK', 'Web Development with Big Data'),
-('HUH2101ACS', 6182301024, 'HUH', 'Database Design and Optimization'),
-('ELH2390CCS', 6182301032, 'ELH', 'Mobile Application Development'),
-('HUH2102BCS', 6182101099, 'HUH', 'RESTful API Design');
+('GDK5801ADS', '6182301004', 'GDK', 'Web Development with Big Data'),
+('HUH2101ACS', '6182301024', 'HUH', 'Database Design and Optimization'),
+('ELH2390CCS', '6182301032', 'ELH', 'Mobile Application Development'),
+('HUH2102BCS', '6182101099', 'HUH', 'RESTful API Design');
 
 -- Insert dummy data into GuidanceSchedule table
 INSERT INTO GuidanceSchedule (topicCode, day, hourStart, hourEnd, notes, place) VALUES
