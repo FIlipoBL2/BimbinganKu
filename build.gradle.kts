a8a05ccedf9068ac88368e15cb642d76b7c9171f
Filipo Bintang Lautan <6182301004@student.unpar.ac.id>
2025-12-10 14:06:05 +0700

    fix: Revert Java version to 21 for compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ description = "Project Bimbingan TA1 TA2 Bagi mahasiswa UNPAR"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(24)
+		languageVersion = JavaLanguageVersion.of(21)
 	}
 }
 
